d9da621f0bdceb70aef823757e6d26113b01eaea
Juan Carlos Gómez M <juan.gomez-le@zoluxiones.com>
2019-12-23 12:05:41 -0500

    Con el tooltipo del timeline terminado

--- src/app/views/dashboards/timeline/timeline.component.ts
+++ src/app/views/dashboards/timeline/timeline.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
 
 @Component({
   selector: 'timeline',
@@ -8,5 +10,22 @@ import { Component } from '@angular/core';
 export class timelinecomponent { 
 
   public TieneIntegral:boolean = true;
+  public msjcont : string = 
+  "Ctn 01: SUDU1847236 / 09-12-2019 10:04 " + 
+  "Ctn 11: SUDU1847236 / 09-12-2019 20:04 " +
+  "Ctn 12: SUDU1847236 / 09-12-2019 23:04 " +
+  "Ctn 13: SUDU1847236 / 09-12-2019 10:04 " +
+  "Ctn 01: SUDU1847236 / 09-12-2019 10:04 " + 
+  "Ctn 11: SUDU1847236 / 09-12-2019 20:04 " +
+  "Ctn 12: SUDU1847236 / 09-12-2019 23:04 " +
+  "Ctn 13: SUDU1847236 / 09-12-2019 10:04 " +
+  "Ctn 01: SUDU1847236 / 09-12-2019 10:04 " + 
+  "Ctn 11: SUDU1847236 / 09-12-2019 20:04 " +
+  "Ctn 12: SUDU1847236 / 09-12-2019 23:04 " +
+  "Ctn 13: SUDU1847236 / 09-12-2019 10:04 " +
+  "Ctn 01: SUDU1847236 / 09-12-2019 10:04 " + 
+  "Ctn 11: SUDU1847236 / 09-12-2019 20:04 " +
+  "Ctn 12: SUDU1847236 / 09-12-2019 23:04 " +
+  "Ctn 13: SUDU1847236 / 09-12-2019 10:04 " +
+  "Ctn 14: SUDU1847236 / 09-12-2019 10:04 ";
 }
-
